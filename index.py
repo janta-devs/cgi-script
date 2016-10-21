53fb8cb9193a5d919d5ede714379977ebafdc63c
Anto-Linux <jadzantonyngayo@gmail.com>
2016-10-21 16:06:08 +0300

    initial commit

---
+++ index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+print "Content-type: text/html\n\n"
+print "<html>\n<body>"
+print "<div style=\"width: 100%; font-size: 40px; font-weight: bold; text-align: center;\">"
+print "Python Script Test Page"
+print "</div>\n</body>\n</html>"
+
+
+
